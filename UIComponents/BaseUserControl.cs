65673cda7fe8876f40f86069a9ef51e4b19868c7
a1098990276-hue <a1098990276@gmail.com>
2026-04-01 03:45:23 +0300

    Add BaseUserControl helper class

---
+++ UIComponents/BaseUserControl.cs
@@ -0,0 +1,16 @@
+// BaseUserControl.cs
+
+using System.Windows.Controls;
+
+namespace MyOrg.SAQR_ERP.UIComponents
+{
+    public class BaseUserControl : UserControl
+    {
+        public BaseUserControl()
+        {
+            // Constructor logic here
+        }
+
+        // Add common properties and methods for user controls
+    }
+}
