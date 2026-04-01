b3b88d6ff2d40c528ac8967181e12015a389c1f9
a1098990276-hue <a1098990276@gmail.com>
2026-04-01 07:35:41 +0300

    Create main.ts for simple NestJS application

---
+++ Core/src/main.ts
@@ -0,0 +1,11 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  app.enableCors(); // Enable CORS if needed
+  app.listen(3000, '0.0.0.0');
+  console.log('Application is running on: http://0.0.0.0:3000');
+}
+
+bootstrap();
