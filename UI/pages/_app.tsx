888397603069138e7ddf19b96ce87c8d64583f06
a1098990276-hue <a1098990276@gmail.com>
2026-04-01 07:58:34 +0300

    Create _app.tsx file in UI/pages

---
+++ UI/pages/_app.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import type { AppProps } from 'next/app';
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <style>{` * {
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+      }
+      
+      html, body {
+        width: 100%;
+        height: 100%;
+      }
+      
+      body {
+        font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
+          'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',
+          sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+      }
+      `}</style>
+      <Component {...pageProps} />
+    </>
+  );
+}
+
+export default MyApp;
